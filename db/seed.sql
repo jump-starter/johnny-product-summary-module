3a818f3c4aa8460aea413dffc88797de46b29416
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-05-02 20:01:26 -0700

    Seed postgres with 100 fake entries

--- db/seed.sql
+++ db/seed.sql
@@ -4,7 +4,7 @@ CREATE DATABASE jumpstarter;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
-  created_at timestamp with time zone,
+  created_at timestamp with time zone DEFAULT current_timestamp,
   name varchar(50) NOT NULL,
   email varchar(50) NOT NULL,
   location varchar(50) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE users (
 
 CREATE TABLE projects (
   id serial PRIMARY KEY,
-  created_at timestamp with time zone,
+  created_at timestamp with time zone DEFAULT current_timestamp,
   end_date timestamp with time zone,
   title varchar(50) NOT NULL,
   description varchar(255) NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE projects (
 
 CREATE TABLE faq (
   id serial PRIMARY KEY,
-  created_at timestamp with time zone,
+  created_at timestamp with time zone DEFAULT current_timestamp,
   title varchar (255) NOT NULL,
   description varchar (255) NOT NULL,
   project_id integer references projects(id)
@@ -32,7 +32,7 @@ CREATE TABLE faq (
 
 CREATE TABLE updates (
   id serial PRIMARY KEY,
-  created_at  timestamp with time zone,
+  created_at  timestamp with time zone DEFAULT current_timestamp,
   title varchar(255) NOT NULL,
   description varchar(255) NOT NULL,
   project_id integer references projects(id)
@@ -40,7 +40,7 @@ CREATE TABLE updates (
 
 CREATE TABLE comments (
   id serial PRIMARY KEY,
-  created_at timestamp with time zone,
+  created_at timestamp with time zone DEFAULT current_timestamp,
   description varchar(255) NOT NULL,
   project_id integer references projects(id),
   user_id integer references users(id)
@@ -48,15 +48,15 @@ CREATE TABLE comments (
 
 CREATE TABLE reminders (
   id serial PRIMARY KEY,
+  created_at timestamp with time zone DEFAULT current_timestamp,
   project_id integer references projects(id),
   user_id integer references users(id)
 );
 
 CREATE TABLE projects_users_pledges (
   id serial PRIMARY KEY,
+  created_at timestamp with time zone DEFAULT current_timestamp,
   pledge_amt decimal,
   project_id integer references projects(id),
   user_id integer references users(id)
 );
-
-\c postgres;
--- db/seedPostgres.js
+++ db/seedPostgres.js
@@ -4,15 +4,22 @@ const db = require('./index.js');
 async function seedPostgres() {
   await db.client.connect();
 
-  await db.client.query(`DELETE FROM users`);
+  await db.client.query('DELETE FROM users');
 
-  for (let i = 0; i < 1000; i += 1) {
-    await db.client.query(`INSERT INTO users (name, email, location, avatar_url) VALUES ('zhujohnny', 'zhujohnny@gmail.com', 'San Francisco', 'www.zhujohnny.com')`);
+  for (let i = 0; i < 100; i += 1) {
+    db.client.query(`INSERT INTO users (name, email, location, avatar_url) 
+      VALUES (
+        '${faker.name.firstName()} ${faker.name.lastName()}',
+        '${faker.internet.email()}',
+        '${faker.address.city()}',
+        '${faker.image.imageUrl()}'
+      )
+    `);
   }
 
-  const res = await db.client.query(`SELECT * FROM users`);
-  console.log(res.rows.length) // Hello world!
-  
+  const res = await db.client.query('SELECT * FROM users');
+  console.log(res.rows.length);
+
   await db.client.end();
 }
 
