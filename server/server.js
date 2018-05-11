f1ded0866f16fa7e6a58ca38472d7c14dd6ded60
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-05-10 21:18:50 -0700

    Implemented redis

--- server/server.js
+++ server/server.js
@@ -5,11 +5,18 @@ const cluster = require('cluster');
 const os = require('os');
 const cors = require('cors');
 const camel = require('to-camel-case');
+const redis = require('redis');
 const db = require('../db');
 // const seedMongo = require('../db/seedMongo');
 // const seedPostgres = require('../db/seedPostgres');
 // seedMongo();
 // seedPostgres();
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+  console.log(`Error ${err}`);
+});
 
 if (cluster.isMaster) {
   const cpuCount = os.cpus().length;
@@ -25,30 +32,38 @@ if (cluster.isMaster) {
 
   const port = 3001;
 
-  app.get('/api/:id', async (req, res) => {
-    const results = {};
-
+  app.get('/api/:id', (req, res) => {
     const { id } = req.params;
-    const { rows } = await db.query(`SELECT *
-                                     FROM projects 
-                                     WHERE id = ${id}`);
-    Object.keys(rows[0]).forEach((key) => {
-      results[camel(key)] = rows[0][key];
+
+    client.get(id, async (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        const results = {};
+
+        const { rows } = await db.query(`SELECT *
+                                         FROM projects 
+                                         WHERE id = ${id}`);
+        Object.keys(rows[0]).forEach((key) => {
+          results[camel(key)] = rows[0][key];
+        });
+
+        const toAppend1 = await db.query(`SELECT first_name AS "firstName", last_name AS "lastName", avatar_url AS "avatarUrl"
+                                         FROM users
+                                         WHERE id = ${results.creatorId}`);
+        results.firstName = toAppend1.rows[0].firstName;
+        results.lastName = toAppend1.rows[0].lastName;
+        results.avatarUrl = toAppend1.rows[0].avatarUrl;
+
+        const toAppend2 = await db.query(`SELECT count(*) AS "projectsCreatedCount"
+                                          FROM projects
+                                          WHERE id = ${results.creatorId}`);
+        results.projectsCreatedCount = Number(toAppend2.rows[0].projectsCreatedCount);
+
+        client.set(id, results);
+        res.send(results);
+      }
     });
-
-    const toAppend1 = await db.query(`SELECT first_name AS "firstName", last_name AS "lastName", avatar_url AS "avatarUrl"
-                                     FROM users
-                                     WHERE id = ${results.creatorId}`);
-    results.firstName = toAppend1.rows[0].firstName;
-    results.lastName = toAppend1.rows[0].lastName;
-    results.avatarUrl = toAppend1.rows[0].avatarUrl;
-
-    const toAppend2 = await db.query(`SELECT count(*) AS "projectsCreatedCount"
-                                      FROM projects
-                                      WHERE id = ${results.creatorId}`);
-    results.projectsCreatedCount = Number(toAppend2.rows[0].projectsCreatedCount);
-
-    res.send(results);
   });
 
   app.listen(port, () => {
