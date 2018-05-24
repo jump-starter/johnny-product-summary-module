2417951e5d41011bcedaac00e7812102204a0d4b
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-05-24 14:41:05 -0700

    Updates load testing probabilities for 80/20 rule

--- wrk.lua
+++ wrk.lua
@@ -8,8 +8,10 @@ math.randomseed(os.time())
 -- the request function that will run at each request
 request = function() 
    
-  -- define the path that will be searched, picking a random project_id between 0 and 10000000)
-   url_path = "/api/" .. (1 + math.floor(10000000*math.random()^10))
+  -- define the path that will be searched, picking a random project_id with:
+    -- 80% probability of being between 9M and 10M
+    -- 20% probability of being between 1M - 9M
+   url_path = "/api/" .. (math.random() < 0.8 and 9e6 + math.floor(1e6 * (math.random())) or 1 + math.floor(9e6 * (math.random())))
 
   -- Return the request object with the current URL path
    return wrk.format("GET", url_path)
